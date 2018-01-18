6c951b10538c41fe7b61face60079ae4564352b8
arintaosorio <arintaosorio@gmail.com>
2018-01-18 10:08:00 -0600

    primeras funciones jquery

--- js/app.js
+++ js/app.js
@@ -1,10 +1,27 @@
 
   //Traer elementos del HTML
+  var $nameInput = $("#name-input");
+  var $phoneInput = $("#phone-input");
+  
+
   
   
   var loadPage = function () {
-    
-  };
+    $(".modal").modal();
+    $nameInput.keyup(validateContact);
+    $phoneInput.keyup(validateContact);
+    };
+
+  var validateContact = function () {
+    //En esta funcion tenemos que validar que el usuario ingrese datos y no valores vacios
+    var $containerAddContact = $("#add-contact");
+    if($(this).val().trim().length > 0){
+      $containerAddContact.removeAttr("disabled");
+    }  else{
+      $containerAddContact.Attr("disabled", true);
+    }
+  }; 
+
 
   var paintContactsInHTML = function(contact) {
    
@@ -40,9 +57,6 @@
   
 
 
-  var validateContact = function () {
-    //En esta funcion tenemos que validar que el usuario ingrese datos y no valores vacios
-  };
 
 
   var filterContacts = function() {
